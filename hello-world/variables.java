f3ec3705ff5630a7d940a4224b99a1e37c7e3fce
griffith <griffithawuah15@gmail.com>
2019-09-15 16:11:17 +0000

    [add] add variable lessons

---
+++ hello-world/variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+	public static void main(String[] args) {
+		System.out.println("Introduction To Variables");
+
+		int firstVariable = 10;
+		int secondVariable = 30;
+
+		int sumOfTwoVariables = firstVariable + secondVariable;
+
+		System.out.println(sumOfTwoVariables)
+	}
+}
