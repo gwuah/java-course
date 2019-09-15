2884d43af090ca3618c7b2b9bb73590b2a363f06
griffith <griffithawuah15@gmail.com>
2019-09-15 18:44:21 +0000

    [add] floats and doubles

---
+++ variables/floatDouble.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    double myInt = 5;
+    System.out.println(myInt);
+
+    float newFloat= (float) 5.4;
+    System.out.println(newFloat);
+  }
+}
---
+++ variables/poundsToKgConverter.java
@@ -0,0 +1,11 @@
+public class PoundsToKGConverter {
+	public static void main(String[] args) {
+		double rate = 0.45359237;7
+
+		int poundsToBeConverted = 5;
+
+		double converted = poundsToBeConverted * rate;
+
+		System.out.println(converted)
+	}
+}
