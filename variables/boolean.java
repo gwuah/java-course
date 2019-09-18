9749620642e9d3ee4f59c53a682b8220830ae85d
griffith <griffithawuah15@gmail.com>
2019-09-18 09:49:34 +0000

    [add] added boolean and char lesson

---
+++ variables/boolean.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Boolean Lesson");
+
+		boolean isCustomer = true ;
+
+		boolean showDialog = false;
+
+		System.println("isCustomer" + isCustomer);
+
+	}
+}
---
+++ variables/char.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Character Lesson");
+
+		// can only handle a single character
+		char myChar = "4";
+
+		// but you can use a unicode char too
+
+		char myUnicode = "\u0092"
+
+	}
+}
