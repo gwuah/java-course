2ca1ef883379e7e972e3e4b89c7ef86654a092e0
griffith <griffithawuah15@gmail.com>
2019-09-25 13:25:42 +0000

    [add] added lessons on operators and strings

---
+++ variables/operators.java
@@ -0,0 +1,23 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Testing out operators");
+
+		int passMark = 80;
+
+		int myScore = 70;
+
+		if (myScore < passMark) {
+			System.out.println("You failed");
+		} else {
+			System.out.println("You passed");
+		}
+
+		boolean isAlien = false;
+
+		if (isAlien) {
+			System.out.println("I am an alien");
+		} else {
+			System.out.println("Hey Human");
+		}
+	}
+}
---
+++ variables/string.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+	System.out.println("\nIntroduction To Strings");
+
+    String name = "Griffith Asare Awuah";
+
+    System.out.println("My name is " + name);
+	}
+}
